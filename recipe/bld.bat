2453c88600f683ec8b189db08b64f9a2f83d05ae
Wolf Vollprecht <w.vollprecht@gmail.com>
2019-04-16 11:39:08 +0200

    release 0.7.4 experimental

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -5,6 +5,8 @@ cmake -G "NMake Makefiles" ^
       -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
       -D CMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
       -D CMAKE_VERBOSE_MAKEFILE:BOOL=ON ^
+      -D ENABLE_CONDA:BOOL=ON ^
+      -D MULTI_SEMANTICS:BOOL=ON ^
       -D DISABLE_SHARED=ON ^
       ..
       
--- recipe/build.sh
+++ recipe/build.sh
@@ -5,6 +5,8 @@ cd build
 cmake -DCMAKE_INSTALL_PREFIX=$PREFIX \
       -DCMAKE_PREFIX_PATH=$PREFIX \
       -DCMAKE_INSTALL_LIBDIR=lib \
+      -DENABLE_CONDA=ON \
+      -DMULTI_SEMANTICS=ON \
       ..
 
 make VERBOSE=1 -j${CPU_COUNT}
