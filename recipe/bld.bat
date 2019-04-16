930e2c337b36b8fecebfda8ce9de8c3c14cb6b8b
Wolf Vollprecht <w.vollprecht@gmail.com>
2019-04-16 12:14:30 +0200

    ..

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -4,10 +4,11 @@ cd build
 cmake -G "NMake Makefiles" ^
       -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
       -D CMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
-      -D CMAKE_VERBOSE_MAKEFILE:BOOL=ON ^
-      -D ENABLE_CONDA:BOOL=ON ^
-      -D MULTI_SEMANTICS:BOOL=ON ^
+      -D CMAKE_VERBOSE_MAKEFILE=ON ^
+      -D ENABLE_CONDA=ON ^
+      -D MULTI_SEMANTICS=ON ^
       -D DISABLE_SHARED=ON ^
+      -D WITHOUT_COOKIEOPEN=ON ^
       ..
       
 if errorlevel 1 exit 1
